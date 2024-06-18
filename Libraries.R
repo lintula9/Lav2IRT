83c9becfc09deb3c928c699e1cbd444bea914d5c
lintula9 <123648372+lintula9@users.noreply.github.com>
2024-06-18 17:45:34 +0300

    aaa

--- Libraries.R
+++ Libraries.R
@@ -2,8 +2,6 @@
 package_names <- c("lavaan", "mirt", "pbapply", "RColorBrewer", "ggplot2", "StanHeaders", "Rcpp", "BH", "RcppEigen", "RcppParallel",
                    "devtools")
 
-if( !requireNamespace("StanHeaders", quietly = F) ) install.packages("StanHeaders", repos = c("https://mc-stan.org/r-packages/", getOption("repos")))
-
 for (i in package_names){
   if ( !requireNamespace( i, 
                           quietly = T )) {
@@ -12,18 +10,3 @@ for (i in package_names){
   }
   library( i, character.only = TRUE )}
 
-# Graphical settings --
-# cols <- brewer.pal(n = 8, name = "Dark2")[1:3]
-# names(cols) <- c("Total", "Male", "Female")
-# theme_set(theme_bw())
-# options(ggplot2.discrete.colour= cols)
-# par(family = "serif")
-
-
-# if(!requireNamespace("autodiffr", quietly = F)) devtools::install_github("Non-Contradiction/autodiffr")
-# if(!requireNamespace("JuliaCall", quietly = F)) devtools::install_github("Non-Contradiction/JuliaCall")
-
-# library(JuliaCall); library(autodiffr)
-
-## THIS NEEDS UPDATING TO MORE GENERAL OR SOMETHING THAT CHECKS IF JULIA IS IN PATH?
-# ad_setup(JULIA_HOME = "/Program Files/Julia-1.10.2/bin/")
